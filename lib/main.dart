705dfc38eabf6db9d8b8b9528674748f5d50847b
Gyeongtae Kim <plodio.dev@gmail.com>
2021-02-10 23:20:06 +0900

    english words package 임포트

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("App Loading");
+    nouns.take(30).forEach((String element) {
+      print(element);
+    }); //  47m 20s
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
