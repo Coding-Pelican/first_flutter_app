8a8eb3c2ff2c04a8b78f4ad6d028b63f22ccc35a
Gyeongtae Kim <plodio.dev@gmail.com>
2021-02-13 23:29:09 +0900

    리스트 뷰 추가

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,40 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("App Loading");
-    nouns.take(30).forEach((String element) {
-      print(element);
-    }); //  47m 20s
+    WordPair wordPair = WordPair.random();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("First Flutter App"),
         ),
-        body: Center(child: Text("This is Body of my First Flutter App")),
+        body: Center(child: RandomWords()), //  Alt + Enter
       ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  List<WordPair> words = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        if (index >= words.length) words.addAll(generateWordPairs().take(10));  //  1h 9m
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            words[index].asCamelCase,
+            textScaleFactor: 1,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        );
+      },
     );
   }
 }
